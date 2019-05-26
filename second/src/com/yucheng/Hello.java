108860412722a39d998bbc16e4c9f25c7d3f4593
dingding <914104723@qq.com>
2019-05-26 12:37:21 +0800

    first commit

---
+++ second/src/com/yucheng/Hello.java
@@ -0,0 +1,12 @@
+package com.yucheng;
+
+/**
+ * Created by Administrator on 2019/5/25 0025.
+ */
+public class Hello {
+    public static final int INS = 1;
+
+    int s = 1;
+    int z = 2;
+
+}
---
+++ secondweb/javademo/src/com/yucheng/FirstDemo.java
@@ -0,0 +1,7 @@
+package com.yucheng;
+
+/**
+ * Created by Administrator on 2019/5/26 0026.
+ */
+public class FirstDemo {
+}
