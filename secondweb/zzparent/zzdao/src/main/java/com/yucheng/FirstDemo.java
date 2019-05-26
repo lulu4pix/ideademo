a73bce909821940603b94f18ccdf00b9104d9437
dingding <914104723@qq.com>
2019-05-26 12:35:09 +0800

    first commit

---
+++ secondweb/zzparent/zzdao/src/main/java/com/yucheng/FirstDemo.java
@@ -0,0 +1,13 @@
+package com.yucheng;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Administrator on 2019/5/26 0026.
+ */
+public class FirstDemo {
+    public static void main(String[] args) {
+        System.out.println("cdsaf");
+    }
+}
